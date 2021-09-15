6bb429a0789d374cc397f6b935bc926813f2760e
Administrator <admin@example.com>
2021-09-14 19:57:33 -0500

    some more updates

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import discord
 import os
-from discord.channel import VoiceChannel
 import requests
-from discord.ext import commands
-from datetime import datetime
 from dotenv import load_dotenv
 from discord import FFmpegPCMAudio
 from youtube_dl import YoutubeDL
 import queue
+import threading
+import uuid
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-# GUILD = os.getenv('DISCORD_GUILD')
 
 def process_command(message):
     message.channel.send('test')
 
 class Music():
+    uuid: str
     name: str
     link: str
     requester: str
 
-    def __init__(self, name, link, requester):
+    def __init__(self, uuid, name, link, requester):
+        self.uuid = uuid
         self.name = name
         self.link = link
         self.requester = requester
@@ -32,70 +32,96 @@ class MusicPlayer():
     voice_client = None
     is_playing = False
 
-    YDL_OPTIONS = {
-        'format': 'bestaudio',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'outtmpl': 'song.%(ext)s',
-    }
+    def checkStatusEverySixtySeconds(self, f_stop):
+        if not self.voice_client == None:
+            print(f'is_playing(): {self.voice_client.is_playing()}')
+            if not self.voice_client.is_playing():
+                if not self.song_queue.empty():
+                    try:
+                        song = self.get_song()
+                        self.play_internal(song)
+                    except ValueError as err:
+                        print(f'Unable to play next song due to: {err}')
 
-    async def player(self):
-        print(f'Music player is not yet implemented')
+            self.is_playing = False
+            self.active_song = None
+
+        if not f_stop.is_set():
+            threading.Timer(5, self.checkStatusEverySixtySeconds, [f_stop]).start()
+
+    def __init__(self):
+        f_stop = threading.Event()
+        self.checkStatusEverySixtySeconds(f_stop)
+        print(f'Music Player has been initialized')
 
     async def createVoiceConnection(self, voice_channel):
         self.voice_client = await voice_channel.connect()
 
     async def destroyVoiceConnection(self, message):
-        # if self.voice_client == None or not self.voice_client.is_connected:
-        #     await message.channel.send(f'Not currently connected to a voice channel')
-        #     return
-
         await self.voice_client.disconnect()
         await message.channel.send(f'Disconnected!')
 
     async def addToQueue(self, message, link, requester):
-        # if self.voice_client == None or not self.voice_client.is_connected:
-        #     await message.channel.send(f'Not currently connected to a voice channel')
-        #     return
-
-        song = Music("Placeholder", link, requester)
+        song = Music(uuid.uuid4(), "Placeholder", link, requester)
         self.song_queue.put(song)
         await message.channel.send(f'[DEBUG] Added <<Placeholder>> to the queue')
-        # If a song is queued and no song is playing it, go ahead and start playing
+
         if not self.is_playing:
             await message.channel.send(f'[DEBUG] Playing <<Placeholder>> from the queue')
             await self.play(message)
 
-    async def play(self, message):
-        # time for magic
-        # First, Check if you url contains 'youtube' - if it does grab it 
+    def get_song(self):
         song = self.song_queue.get()
 
+        YDL_OPTIONS = {
+            'format': 'bestaudio',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': f'{song.uuid}.%(ext)s',
+        }
+
         if "youtube.com" in song.link or "youtu.be" in song.link:
-            with YoutubeDL(self.YDL_OPTIONS) as ydl:
+            with YoutubeDL(YDL_OPTIONS) as ydl:
                 print(f'{song.link}')
                 ydl.download([song.link])
 
         # If not, check if you url is an mp3 and download it
         elif "mp3" in song.link:
             downloaded = requests.get(song.link)
-            with open('song.mp3', 'wb') as f:
-                f.write(downloaded.content)
+            with open(f'{song.uuid}.mp3', 'wb') as f:
+                f.write(downloaded.content)  
 
         else:
-            await message.channel.send(f'For some reason {song.link} does not appear to be a valid link.')
+            raise ValueError(f'{song.link} does not appear to be a valid song link.')
 
+        song
+
+    async def play(self, message):
+        if not self.voice_client.is_connected():
+            await message.channel.send(f'I am not currently in a voice channel. Please join a voice channel and use \`join.')
+            return
+        
+        try:
+            song = self.get_song()
+        except ValueError as err:
+            await message.channel.send(f'Error: {err}' )
+
+        self.play_internal(song)
+
+    def play_internal(self, song):
         if not self.voice_client.is_playing():
             self.is_playing = True
             self.active_song = song
-            self.voice_client.play(FFmpegPCMAudio("song.mp3"))
-            self.voice_client.is_playing()
-            await message.channel.send(f'Now playing: {self.song_queue[0].link}')
+            self.voice_client.play(FFmpegPCMAudio(f"{song.uuid}.mp3"))
 
     async def pause(self, message):
+        if not self.voice_client.is_connected():
+            await message.channel.send(f'I am not currently in a voice channel. Please join a voice channel and use \`join.')
+            return
+
         if not self.active_song == None:
             self.voice_client.pause()
             await message.channel.send(f'Paused')
@@ -103,6 +129,10 @@ class MusicPlayer():
             await message.channel.send(f'There is no currently active song')
 
     async def resume(self, message):
+        if not self.voice_client.is_connected():
+            await message.channel.send(f'I am not currently in a voice channel. Please join a voice channel and use \`join.')
+            return
+
         if not self.active_song == None:
             self.voice_client.resume()
             await message.channel.send(f'Resuming')
@@ -110,6 +140,10 @@ class MusicPlayer():
             await message.channel.send(f'There is no currently active song')
 
     async def skip(self, message):
+        if self.voice_client == None or not self.voice_client.is_connected():
+            await message.channel.send(f'I am not currently in a voice channel. Please join a voice channel and use \`join.')
+            return
+
         if not self.active_song == None:
             self.voice_client.stop()
             self.is_playing = False
@@ -118,9 +152,11 @@ class MusicPlayer():
         else:
             await message.channel.send(f'There is no currently active song')
 
-music_player = MusicPlayer()
-
 class CommandProcessor():
+    # Initialize the music player and begin polling every 60 seconds
+    music_player = MusicPlayer()
+    voice_connection = None
+
     async def processor(self, client, message):
         message_text = message.content.split()
 
@@ -143,27 +179,26 @@ class CommandProcessor():
             await message.channel.send(f'User {message.author.nick} is not currently in a voice channel.')
             return
 
-        await message.channel.send(f'Joining {message.author.voice.channel.name} on behalf of {message.author.nick}')
-        await music_player.createVoiceConnection(message.author.voice.channel)
+        try:
+            self.voice_connection = await self.music_player.createVoiceConnection(message.author.voice.channel)
+            await message.channel.send(f'Joined {message.author.voice.channel.name} on behalf of {message.author.nick}')
+        except Exception as err:
+            await message.channel.send(f'Unable to connect to {message.author.voice.channel.name} due to: {err}')
 
     async def com_disconnect(self,  message, arguments):
-        await music_player.destroyVoiceConnection(message)
+        await self.music_player.destroyVoiceConnection(message)
 
     async def com_pause(self, message, arguments):
-        await music_player.pause(message)
+        await self.music_player.pause(message)
 
     async def com_resume(self, message, arguments):
-        await music_player.resume(message)
+        await self.music_player.resume(message)
 
     async def com_skip(self, message, arguments):
-        await music_player.skip(message)
+        await self.music_player.skip(message)
 
     async def com_add(self, message, arguments):
-        # if(len(arguments) < 2):
-        #     await message.channel.send('Usage: `add [Song Link]')
-        #     return
-        await music_player.addToQueue(message, arguments[0], message.author.nick)
-        # await message.channel.send('TODO')
+        await self.music_player.addToQueue(message, arguments[0], message.author.nick)
 
 commandProcessor = CommandProcessor()
 
@@ -177,10 +212,6 @@ class CustomClient(discord.Client):
     async def on_message(self, message):
         if message.author == client.user:
             return
-
-        # print(f'{message}')
-        # print(f'{message.content}')
-        # print(f'{message.author.voice}')
 
         if message.content.startswith("`"):
             await commandProcessor.processor(self, message)
